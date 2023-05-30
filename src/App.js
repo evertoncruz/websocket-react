0683b3f8455d10377a8047ec1d08ece8f8505066
Everton Cruz <cruz.everton@gmail.com>
2023-05-30 19:55:07 -0300

    websocket integrated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import useWebSocket from 'react-use-websocket';
+import { useState } from 'react';
 
 function App() {
+  const { lastJsonMessage, sendMessage } = useWebSocket('ws://localhost:3001', {
+    onOpen: () => console.log(`Connected to App WS`),
+    onMessage: () => {
+      if (lastJsonMessage) {
+        console.log(lastJsonMessage);
+        setNumero(lastJsonMessage.n);
+      }
+    },
+    queryParams: { 'token': '123456' },
+    onError: (event) => { console.error(event); },
+    shouldReconnect: (closeEvent) => true,
+    reconnectInterval: 3000
+  });
+
+  const [numero, setNumero] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {numero}
       </header>
     </div>
   );
